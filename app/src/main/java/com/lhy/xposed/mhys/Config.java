35ee896e8cbb672f25007dd88586c9bcba81b65a
1595901624 <35366425+1595901624@users.noreply.github.com>
2019-02-18 13:14:21 +0800

    Rename

--- app/src/main/java/com/lhy/xposed/mhys/Config.java
+++
@@ -1,7 +0,0 @@
-package com.lhy.xposed.mhys;
-
-public class Config {
-    public static boolean IS_FIRST_RUN = true;
-    public static int TEST = 0;
-    public static final String HOOK_APPLICATION_PACKAGE_NAME = "com.amahua.ompimdrt";
-}
--- app/src/main/java/com/lhy/xposed/mhys/plugin/IPlugin.java
+++
@@ -1,7 +0,0 @@
-package com.lhy.xposed.mhys.plugin;
-
-import de.robv.android.xposed.XC_MethodHook;
-
-public interface IPlugin {
-    public void run(ClassLoader classLoader) throws Throwable;
-}
