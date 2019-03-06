646ef95b3b6ce3eaa271b88527c64a215c9f077b
1595901624 <35366425+1595901624@users.noreply.github.com>
2019-03-06 16:45:11 +0800

    Support Edxposed

--- app/src/main/java/com/lhy/xposed/mhzs/HookLoader.java
+++ app/src/main/java/com/lhy/xposed/mhzs/HookLoader.java
@@ -4,7 +4,6 @@ import android.util.Log;
 
 import com.lhy.xposed.mhzs.helper.Config;
 import com.lhy.xposed.mhzs.helper.LogUtil;
-import com.lhy.xposed.mhzs.helper.XPrefUtils;
 
 import java.io.File;
 import java.lang.reflect.Method;
@@ -22,44 +21,40 @@ public class HookLoader implements IXposedHookLoadPackage {
         if (!Config.HOOK_APPLICATION_PACKAGE_NAME.equals(loadPackageParam.packageName))
             return;
 
-        HookMain hookMain = new HookMain();
-        hookMain.handleLoadPackage4release(loadPackageParam);
+        try {
+            LogUtil.e("123123");
+            //在发布时，直接调用即可。
+            if (!BuildConfig.DEBUG) {
+                HookMain hookMain = new HookMain();
+                hookMain.handleLoadPackage4release(loadPackageParam);
+                return;
+            }
+            //在调试模式为了不频繁重启，使用反射的方式调用自身的指定函数。
 
-//        try {
-//            //在发布时，直接调用即可。
-//            if (!BuildConfig.DEBUG) {
-//                HookMain hookMain = new HookMain();
-//                hookMain.handleLoadPackage4release(loadPackageParam);
-//                return;
-//            }
-//            //在调试模式为了不频繁重启，使用反射的方式调用自身的指定函数。
-//
-//            /*【方法2】*/
-//            final String packageName = HookMain.class.getPackage().getName();
-//            String filePath = String.format("/data/app/%s-%s.apk", packageName, 1);
-//            if (!new File(filePath).exists()) {
-//                filePath = String.format("/data/app/%s-%s.apk", packageName, 2);
-//                if (!new File(filePath).exists()) {
-//                    filePath = String.format("/data/app/%s-%s/base.apk", packageName, 1);
-//                    if (!new File(filePath).exists()) {
-//                        filePath = String.format("/data/app/%s-%s/base.apk", packageName, 2);
-//                        if (!new File(filePath).exists()) {
-//                            XposedBridge.log("Error:在/data/app找不到APK文件" + packageName);
-//                            return;
-//                        }
-//                    }
-//                }
-//            }
-//            HookMain hookMain = new HookMain();
-//            hookMain.handleLoadPackage4release(loadPackageParam);
-//
-//            final PathClassLoader pathClassLoader = new PathClassLoader(filePath, ClassLoader.getSystemClassLoader());
-//            final Class<?> aClass = Class.forName(packageName + "." + HookMain.class.getSimpleName(), true, pathClassLoader);
-//            final Method aClassMethod = aClass.getMethod("handleLoadPackage4release", XC_LoadPackage.LoadPackageParam.class);
-//            aClassMethod.invoke(aClass.newInstance(), loadPackageParam);
-//        } catch (final Exception e) {
-//            XposedBridge.log(e);
-//        }
+            /*【方法2】*/
+            final String packageName = HookMain.class.getPackage().getName();
+            String filePath = String.format("/data/app/%s-%s.apk", packageName, 1);
+            if (!new File(filePath).exists()) {
+                filePath = String.format("/data/app/%s-%s.apk", packageName, 2);
+                if (!new File(filePath).exists()) {
+                    filePath = String.format("/data/app/%s-%s/base.apk", packageName, 1);
+                    if (!new File(filePath).exists()) {
+                        filePath = String.format("/data/app/%s-%s/base.apk", packageName, 2);
+                        if (!new File(filePath).exists()) {
+                            XposedBridge.log("Error:在/data/app找不到APK文件" + packageName);
+                            return;
+                        }
+                    }
+                }
+            }
+
+            final PathClassLoader pathClassLoader = new PathClassLoader(filePath, ClassLoader.getSystemClassLoader());
+            final Class<?> aClass = Class.forName(packageName + "." + HookMain.class.getSimpleName(), true, pathClassLoader);
+            final Method aClassMethod = aClass.getMethod("handleLoadPackage4release", XC_LoadPackage.LoadPackageParam.class);
+            aClassMethod.invoke(aClass.newInstance(), loadPackageParam);
+        } catch (final Exception e) {
+            XposedBridge.log(e);
+        }
     }
 
 }
--- app/src/main/java/com/lhy/xposed/mhzs/helper/Config.java
+++ app/src/main/java/com/lhy/xposed/mhzs/helper/Config.java
@@ -3,7 +3,7 @@ package com.lhy.xposed.mhzs.helper;
 public class Config {
     public static boolean IS_FIRST_RUN = true;
     public static int TEST = 0;
-    public static final String HOOK_APPLICATION_PACKAGE_NAME = "com.amahua.ompimdrt";
+    public static final String HOOK_APPLICATION_PACKAGE_NAME = "com.amahua.ywofnbfd";
 
     public static boolean MHZS_GLOBAL_SET = true;
 }
