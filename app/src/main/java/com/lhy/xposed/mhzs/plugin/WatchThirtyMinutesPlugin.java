3719b39dbdc18a995bbee218dc7b29ef708fd1bd
1595901624 <35366425+1595901624@users.noreply.github.com>
2019-03-19 16:12:47 +0800

    Update 0.6.12 Beta

--- app/src/main/java/com/lhy/xposed/mhzs/plugin/WatchThirtyMinutesPlugin.java
+++ app/src/main/java/com/lhy/xposed/mhzs/plugin/WatchThirtyMinutesPlugin.java
@@ -1,5 +1,6 @@
 package com.lhy.xposed.mhzs.plugin;
 
+import android.app.Activity;
 import android.app.Application;
 import android.content.Context;
 import android.content.SharedPreferences;
@@ -26,35 +27,51 @@ public class WatchThirtyMinutesPlugin implements IPlugin {
     @Override
     public void run(ClassLoader classLoader) throws Throwable {
 
-        XposedHelpers.findAndHookMethod(Constant.$MHApplication, classLoader, "onCreate",
-                new XC_MethodHook() {
-                    @Override
-                    protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-                        super.afterHookedMethod(param);
-                        Application application = (Application) param.thisObject;
-                        SharedPreferences sp = application.getSharedPreferences("config", Context.MODE_PRIVATE);
-                        sp.edit().putLong("total_time", 1800002).commit();
-                        LogUtil.e("Hook Movie Time");
-                    }
-                });
+//        XposedHelpers.findAndHookMethod(Constant.$MHApplication, classLoader, "onCreate",
+//                new XC_MethodHook() {
+//                    @Override
+//                    protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+//                        super.afterHookedMethod(param);
+//                        Application application = (Application) param.thisObject;
+//                        SharedPreferences sp = application.getSharedPreferences("config", Context.MODE_PRIVATE);
+//                        sp.edit().putLong("total_time", 1800002).commit();
+//                        LogUtil.e("Hook Movie Time");
+//                    }
+//                });
+//
+//        XposedHelpers.findAndHookMethod(Timer.class.getName(), classLoader, "schedule", TimerTask.class,
+//                long.class, long.class, new XC_MethodHook() {
+//                    @Override
+//                    protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+//                        super.beforeHookedMethod(param);
+//                        long delay = (long) param.args[1];
+//                        long period = (long) param.args[2];
+//                        if (delay == 180000 && period == 180000) {
+//                            param.args[1] = 0;
+//                        }
+//                    }
+//
+//                    @Override
+//                    protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+//                        super.afterHookedMethod(param);
+//                    }
+//                });
 
-        XposedHelpers.findAndHookMethod(Timer.class.getName(), classLoader, "schedule", TimerTask.class,
-                long.class, long.class, new XC_MethodHook() {
-                    @Override
-                    protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-                        super.beforeHookedMethod(param);
-                        long delay = (long) param.args[1];
-                        long period = (long) param.args[2];
-                        if (delay == 180000 && period == 180000) {
-                            param.args[1] = 0;
-                        }
-                    }
+        XposedHelpers.findAndHookMethod(Constant.act.$PlayerActivity, classLoader, "j", new XC_MethodHook() {
+            @Override
+            protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                super.beforeHookedMethod(param);
+            }
 
-                    @Override
-                    protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-                        super.afterHookedMethod(param);
-                    }
-                });
+            @Override
+            protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                super.afterHookedMethod(param);
+                Activity activity = (Activity) param.thisObject;
+                Object pres = XposedHelpers.getObjectField(activity, "b");
+                XposedHelpers.callMethod(pres, "r");
+                LogUtil.e("!321312");
+            }
+        });
     }
 
     @Override
